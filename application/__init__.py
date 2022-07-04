091814adf3ebb03a4264648bdafc6dbf386bf88c
randomrevised <r_evelynkoo@berkeley.edu>
2022-07-04 09:33:55 -0700

    some minor flask setup

---
+++ application/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+# Globally accessible libraries
+db = SQLAlchemy()
+
+
+def init_app():
+    """Initialize the core application."""
+    app = Flask(__name__)  # add config later
+
+    # Initialize Plugins
+    db.init_app(app)
+
+    with app.app_context():
+        # Include our Routes
+        # from . import routes
+
+        return app
