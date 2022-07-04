ae33f4684dcea0aebadeb6652f8f926f9b9e5c2a
randomrevised <r_evelynkoo@berkeley.edu>
2022-07-04 12:15:39 -0700

    added some small scripts

---
+++ scripts/cron.sh
@@ -0,0 +1,8 @@
+cd /mnt/c/Users/Evelyn/Documents/tonk/fund-vis-v2
+cd src
+python3 fund.py cron
+cd ..
+message=$(date '+%Y-%m-%d %H:%M:%S')
+git add src/saved/temp.csv
+git commit -m "add entry at ${message}"
+echo "cron commit at ${message}" >> scripts/cronlog.txt
