de59c9df660d378348becb5d4d4b2985c6d32110
randomrevised <r_evelynkoo@berkeley.edu>
2022-07-06 14:21:00 -0700

    saved

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -6,3 +6,9 @@ message=$(date '+%Y-%m-%d %H:%M:%S')
 git add saved/temp.csv
 git commit -m "add entry at ${message}"
 echo "cron commit at ${message}" >> scripts/cronlog.txt
+git checkout flask-dev
+python3 fund.py cron
+git add saved/temp.csv
+git commit -m "add entry at ${message} - dev branch"
+echo "cron commit at ${message} - dev branch" >> scripts/cronlog.txt
+git checkout main
