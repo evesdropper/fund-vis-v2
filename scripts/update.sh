b79c24fef2db5d77e263dda8459a27fe53db8353
randomrevised <r_evelynkoo@berkeley.edu>
2022-07-06 15:11:56 -0700

    some fixes

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,3 +2,6 @@ cd /mnt/c/Users/Evelyn/Documents/tonk/fund-vis-v2
 message=$(date '+%Y-%m-%d %H:%M:%S')
 git push heroku main
 echo "cron update at ${message}" >> scripts/cronlog.txt
+git push heroku-beta flask-dev:master
+echo "(flask-dev) cron update at ${message}" >> scripts/cronlog.txt
+
